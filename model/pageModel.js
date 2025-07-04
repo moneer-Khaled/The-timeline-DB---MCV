90722eca23faa63a23111600b210ec3548d55c0b
moneer-Khaled <156200343+moneer-Khaled@users.noreply.github.com>
2025-07-04 16:31:03 +0200

    Add files via upload

---
+++ model/pageModel.js
@@ -0,0 +1,21 @@
+const { text } = require('body-parser')
+const { timeStamp } = require('console')
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+
+
+const PostSchema = new Schema({
+      post : {
+        type: String,
+        required: true,
+        min :[25, "Post should be minimum 25 character "]
+      },
+    } , 
+  {timestamps: true}
+  )
+
+const Postmodel =mongoose.model('DBPost',PostSchema)
+
+module.exports= Postmodel
